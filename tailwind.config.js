e1b8a75d6af33dcb4d0e6ceef20487f90a87f916
Adrian Mirel Bonea <88664908+AdrianBonea@users.noreply.github.com>
2022-02-17 21:03:18 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        reactlogo: "url('../src/Images/react-logo-large.jpg')",
+      },
+    },
+
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
